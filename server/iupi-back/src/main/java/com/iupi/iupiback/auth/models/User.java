20a27a9ca00c102b00b5b1fc7825afb9c7ea72ed
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-08 14:01:53 -0500

    Implementación del modelo user

---
+++ server/iupi-back/src/main/java/com/iupi/iupiback/auth/models/User.java
@@ -0,0 +1,65 @@
+package com.iupi.iupiback.auth.models;
+
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    private static final int ID_LENGTH = 18;
+
+    @Id
+    @Column(name = "user_id", nullable = false)
+    private String id;
+
+    @Column(name = "email", nullable = false, unique = true,length = 210)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name",length = 150)
+    private String firstName;
+
+    @Column(name = "last_name",length = 150)
+    private String lastName;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "user_id"),inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "role_id"))
+    private Set<Role> roles;
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = this.getRoles().stream()
+                .map(rol -> new SimpleGrantedAuthority("ROLE_" + rol.getRoleName()))
+                .collect(Collectors.toList());
+
+        // Agregar permisos como autoridades adicionales.
+        List<Permission> permissions = this.getRoles().stream()
+                .flatMap(role -> role.getPermissions().stream())
+                .toList();
+
+        permissions.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getPermissionName())));
+        return authorities;
+    }
+    @PrePersist
+    public void generateId() {
+        if(this.id == null) {
+            this.id = NanoIdUtils.randomNanoId(NanoIdUtils.DEFAULT_NUMBER_GENERATOR, NanoIdUtils.DEFAULT_ALPHABET, ID_LENGTH);
+        }
+    }
+}
