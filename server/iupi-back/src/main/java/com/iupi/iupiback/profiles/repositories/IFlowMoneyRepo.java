8f571d455c1157506979b2ce7fbb9c2b41646b21
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-10 09:26:55 -0500

    repositorio de flujo de dinero

---
+++ server/iupi-back/src/main/java/com/iupi/iupiback/profiles/repositories/IFlowMoneyRepo.java
@@ -0,0 +1,7 @@
+package com.iupi.iupiback.profiles.repositories;
+
+import com.iupi.iupiback.common.repositories.IGenericRepo;
+import com.iupi.iupiback.profiles.models.FlowMoney;
+
+public interface IFlowMoneyRepo extends IGenericRepo<FlowMoney,String> {
+}
