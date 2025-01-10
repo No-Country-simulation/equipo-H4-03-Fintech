777995b42c046acd6d8e9c2b660c6ef27a61a5d4
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-10 09:29:03 -0500

    interfaz de servicio de perfil financiero

---
+++ server/iupi-back/src/main/java/com/iupi/iupiback/profiles/services/IFinancialProfileService.java
@@ -0,0 +1,7 @@
+package com.iupi.iupiback.profiles.services;
+
+import com.iupi.iupiback.common.services.ICRUService;
+import com.iupi.iupiback.profiles.models.FinancialProfile;
+
+public interface IFinancialProfileService extends ICRUService<FinancialProfile,String> {
+}
