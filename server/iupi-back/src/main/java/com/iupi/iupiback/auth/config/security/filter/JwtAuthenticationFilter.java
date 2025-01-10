a92f5390ea9fc1b98e3e067fac4091dc03acda0f
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-09 19:44:57 -0500

    filtro de autentificación

--- server/iupi-back/src/main/java/com/iupi/iupiback/auth/config/security/filter/JwtAuthenticationFilter.java
+++ server/iupi-back/src/main/java/com/iupi/iupiback/auth/config/security/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,21 @@
 package com.iupi.iupiback.auth.config.security.filter;
 
 
+import com.iupi.iupiback.auth.services.IAuthService;
 import com.iupi.iupiback.auth.services.IJwtService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
@@ -12,7 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final IJwtService jwtService;
 
-    private final CustomUserDetailsService userDetailsService;
+    private final IAuthService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
