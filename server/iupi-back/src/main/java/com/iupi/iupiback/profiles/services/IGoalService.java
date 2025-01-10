118029fdca9493f60b66240c6a6b074f86d5b993
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-10 09:28:51 -0500

    interfaz de servicio de objetivos

---
+++ server/iupi-back/src/main/java/com/iupi/iupiback/profiles/services/IGoalService.java
@@ -0,0 +1,7 @@
+package com.iupi.iupiback.profiles.services;
+
+import com.iupi.iupiback.common.services.ICRUService;
+import com.iupi.iupiback.profiles.models.Goal;
+
+public interface IGoalService extends ICRUService<Goal,String> {
+}
