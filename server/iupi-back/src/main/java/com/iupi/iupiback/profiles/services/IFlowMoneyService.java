3606983e79d4be5d06ab68b0e35097327cc38908
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-10 09:28:41 -0500

    interfaz de servicio de flujo de dinero

---
+++ server/iupi-back/src/main/java/com/iupi/iupiback/profiles/services/IFlowMoneyService.java
@@ -0,0 +1,7 @@
+package com.iupi.iupiback.profiles.services;
+
+import com.iupi.iupiback.common.repositories.IGenericRepo;
+import com.iupi.iupiback.profiles.models.FlowMoney;
+
+public interface IFlowMoneyService extends IGenericRepo<FlowMoney,String> {
+}
