a9d05f7dc6f1fecf9d4f182152e2df5ff0cc7deb
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-10 09:26:33 -0500

    repositorio de Flujo de Dinero

---
+++ server/iupi-back/src/main/java/com/iupi/iupiback/profiles/repositories/IFinancialProfile.java
@@ -0,0 +1,7 @@
+package com.iupi.iupiback.profiles.repositories;
+
+import com.iupi.iupiback.common.repositories.IGenericRepo;
+import com.iupi.iupiback.profiles.models.FinancialProfile;
+
+public interface IFinancialProfile extends IGenericRepo<FinancialProfile, String> {
+}
