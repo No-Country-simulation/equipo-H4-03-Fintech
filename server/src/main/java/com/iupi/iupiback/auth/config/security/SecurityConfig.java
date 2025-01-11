3dc00d4452744a15e387ed6aacdaf154b037e7e9
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-11 16:29:37 -0500

    port front cors 5173

--- server/src/main/java/com/iupi/iupiback/auth/config/security/SecurityConfig.java
+++ server/src/main/java/com/iupi/iupiback/auth/config/security/SecurityConfig.java
@@ -158,7 +158,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:3000"));
         configuration.setAllowedMethods(
                 Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"));
         configuration.setAllowedHeaders(List.of("*"));
