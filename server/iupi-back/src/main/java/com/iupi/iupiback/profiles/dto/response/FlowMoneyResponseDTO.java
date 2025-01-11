006c876f48ba26332ce5a02f41adacd9f185c8ec
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-10 20:17:32 -0500

    response de Flujo de Dinero

---
+++ server/iupi-back/src/main/java/com/iupi/iupiback/profiles/dto/response/FlowMoneyResponseDTO.java
@@ -0,0 +1,15 @@
+package com.iupi.iupiback.profiles.dto.response;
+
+import com.iupi.iupiback.profiles.models.enums.FlowType;
+
+import java.time.LocalDate;
+
+public record FlowMoneyResponseDTO(
+        String flowMoneyId,
+        FlowType flowType,
+        String flowMoneyDescription,
+        Double amount,
+        LocalDate date,
+        String urlDocument
+) {
+}
