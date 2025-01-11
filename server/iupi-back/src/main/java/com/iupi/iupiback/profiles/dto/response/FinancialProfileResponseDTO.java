72d3554756c098733a014b071941a27209dc07d7
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-10 20:17:51 -0500

    response de Perfil Financiero

---
+++ server/iupi-back/src/main/java/com/iupi/iupiback/profiles/dto/response/FinancialProfileResponseDTO.java
@@ -0,0 +1,18 @@
+package com.iupi.iupiback.profiles.dto.response;
+
+import com.iupi.iupiback.profiles.models.enums.KnowledgeFinancial;
+import com.iupi.iupiback.profiles.models.enums.RiskProfile;
+import com.iupi.iupiback.profiles.models.enums.SES;
+import com.iupi.iupiback.profiles.models.enums.TypeInvestment;
+
+public record FinancialProfileResponseDTO(
+        String financialProfileId,
+        RiskProfile riskProfile,
+        SES ses,
+        TypeInvestment typeInvestment,
+        KnowledgeFinancial knowledgeFinancial,
+        Double expenses,
+        Double savings,
+        Double debts
+)
+{ }
