49c5075012ba4ef79937fb865c3fe602fa372dc6
Pablo Spector <pablo_spector@hotmail.com>
2025-01-22 02:05:25 -0300

    Eliminado boton para ingreso con apple

--- client/src/components/auth/SocialButtons.jsx
+++ client/src/components/auth/SocialButtons.jsx
@@ -5,14 +5,11 @@ export default function SocialButtons({ auth }) {
   const URL = import.meta.env.VITE_API_GOOGLE_AUTH
 
   return (
-    <section className="w-full flex flex-col gap-5 mb-4">
-      <Link
-        to={URL}
-        className='w-full border border-blue-300 py-2 rounded-3xl text-center'
-      >
-        Google
-      </Link>
-      <button className='w-full border border-gray-300 py-2 rounded-3xl'>apple</button>
-    </section>
+    <Link
+      to={URL}
+      className='w-full border border-blue-300 py-2 rounded-3xl text-center mb-4'
+    >
+      Google
+    </Link>
   )
 }
