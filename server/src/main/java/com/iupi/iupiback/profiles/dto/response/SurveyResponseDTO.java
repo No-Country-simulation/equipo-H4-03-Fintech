6d06f30883e1a572e46d34295f80c09080d33e71
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-28 22:13:47 -0500

    question response dto

--- server/src/main/java/com/iupi/iupiback/profiles/dto/response/SurveyResponseDTO.java
+++ server/src/main/java/com/iupi/iupiback/profiles/dto/response/SurveyResponseDTO.java
@@ -1,11 +1,13 @@
 package com.iupi.iupiback.profiles.dto.response;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public record SurveyResponseDTO(
         int surveyId,
         String title,
         String description,
-        LocalDate startDate
+        LocalDate startDate,
+        List<QuestionResponseDTO> questions
 ) {
 }
