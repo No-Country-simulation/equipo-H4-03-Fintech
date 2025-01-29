63b19ba9c40456ec779ebc12f5acc6ce94298f68
Anderson Cusma <andersoncusma20@gmail.com>
2025-01-29 12:33:39 -0500

    cambios

--- server/src/main/java/com/iupi/iupiback/common/endpoints/FinancialProfileEndpoints.java
+++ server/src/main/java/com/iupi/iupiback/common/endpoints/FinancialProfileEndpoints.java
@@ -3,7 +3,7 @@ package com.iupi.iupiback.common.endpoints;
 public class FinancialProfileEndpoints {
     public static final String[] LIST_ALL_FINANCIAL_PROFILES = {"/api/financial-profile"};
     public static final String[] GET_FINANCIAL_PROFILE = {"/api/financial-profile/user/**"};
-    public static final String[] GEY_MY_FINANCIAL_PROFILE = {"/api/my-financial-profile/me"};
+    public static final String[] GEY_MY_FINANCIAL_PROFILE = {"/api/financial-profile/me"};
     public static final String[] CREATE_FINANCIAL_PROFILE = {"/api/financial-profile"};
     public static final String[] UPDATE_FINANCIAL_PROFILE = {"/api/financial-profile/**"};
     public static final String[] DELETE_FINANCIAL_PROFILE = {"/api/financial-profile/**"};
